de9bd9c5d6505d44251b6be696acf0796bfaa0c3
iris_lai <amy40522a@gmail.com>
2023-06-06 12:05:51 +0800

    update build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/iris-vue3-project/dist/'
 })
